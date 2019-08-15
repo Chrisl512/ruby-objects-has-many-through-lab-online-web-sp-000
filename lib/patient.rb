18ad5aba825b098d6aca0f0c1a1e223cd7e36ebd
Christopher Luna <chris@illusionlayervfx.com>
2019-08-15 04:13:39 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -18,8 +18,8 @@ class Patient
   end
 
   def appointments
-    Appointment.all.collect do |appointment|
-      appointment.doctor
+    Appointment.all.select do |appointment|
+      appointment.patient == self
   end
 
 end
